241b3700c1e18ace10827918ebe6001ec0d1fdb4
Titilope Ajayi oluwaseyi <ajayititilope357@gmail.com>
2026-08-09 22:23:34 +0100

    Add initial body measurement calculations

--- api/measure/app/service/measurement.py
+++ api/measure/app/service/measurement.py
@@ -12,6 +12,13 @@ LEFT_HIP = 23
 RIGHT_HIP = 24
 LEFT_ANKLE = 27
 RIGHT_ANKLE = 28
+LEFT_ELBOW = 13
+RIGHT_ELBOW = 14
+LEFT_WRIST = 15
+RIGHT_WRIST = 16
+LEFT_KNEE = 25
+RIGHT_KNEE = 26
+
 
 
 def pixel_distance(p1, p2, image_width, image_height):
